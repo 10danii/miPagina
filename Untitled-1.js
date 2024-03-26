3b47cae9582d0d7928396c683491e35d62cf5b28
10danii <163131180+10danii@users.noreply.github.com>
2024-03-26 10:38:02 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+let nombre ="daniel"
+let apellido ="acevedo"
+let edad = 17
+let estatura = 1.67
+let conectado = true 
+let desconectado = false 
+let arreglo = ["daniel" , "acevedo" , 17 , 1.67]
+let objeto = {nombre : "daniel" , apellido : "acevedo" , edad : 17 , estatura : 1.67}
---
+++ x.js
@@ -0,0 +1,27 @@
+let nombre ="daniel"
+let apellido ="acevedo"
+let edad = 17
+let estatura = 1.67
+let conectado = true 
+let desconectado = false 
+let arreglito = ["daniel" , "acevedo" , 17 , 1.67]
+let objeto = {nombre : "daniel" , apellido : "acevedo" , edad : 17 , estatura : 1.67}
+
+console.log(objeto)
+console.log(arreglo)
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(estatura)
+console.log(conectado)
+console.log(desconectado)
+
+//declarando los booleanos
+let registrado = false
+let humano = true
+let humanoide = false
+let logeado = true 
+
+// declarando arreglos 
+
+let arreglo = ["daniel"]
